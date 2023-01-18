ddd69b340da460ab2ea277fb81f9e21bf71a4a14
Drake1601 <hieprooney16@gmail.com>
2023-01-18 20:42:57 +0700

    .

---
+++ wiki_crawler/wiki_crawler/pipelines.py
@@ -0,0 +1,36 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+import pymongo
+
+
+class MongoDBPipeline(object):
+    def __init__(self, mongo_server, mongo_port, mongo_db):
+        connection = pymongo.MongoClient(
+            mongo_server,
+            mongo_port,
+        )
+        self.db = connection[mongo_db]
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        settings = crawler.settings
+        return cls(
+            mongo_server = settings.get('MONGO_SERVER'),
+            mongo_port = settings.get('MONGO_PORT'),
+            mongo_db = settings.get('MONGO_DB'),
+        )
+
+    def process_item(self, item, spider):
+        collection = self.db[spider.name]
+        collection.insert_one(dict(item))
+        # log.msg("News added to MongoDB",
+        #         level=log.DEBUG, spider=spider)
+        
+        return item
